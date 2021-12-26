94297bf49fecdbaa27d46ac21cd5b01494f03774
RicoCelsius <72978634+RicoCelsius@users.noreply.github.com>
2021-12-26 10:20:53 +0100

    Added necessary variables

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ API_KEY = ''
 API_KEY_SECRET = '' #Please turn on IP whitelist for extra security.
 SYMBOL = 'LUNAUSDT'
 QUANTITY = 0.4
-GRIDPERC = 0.15
+GRIDPERC = 0.25
 GRIDS = 3
+TG_TOKEN = '' #Wont work yet
+TG_ENABLED = True
 
