913484f2a4fd61c043a076c23fa95b87d1d97423
RicoCelsius <72978634+RicoCelsius@users.noreply.github.com>
2022-01-04 18:15:39 +0100

    Fixed bugs
    Fixed bug with sell orders
    Fixed bug with connectivity check.

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ client = Client(API_KEY, API_KEY_SECRET)
 currentprice = Decimal(client.get_symbol_ticker(symbol=SYMBOL)["price"])
 stepsize = Decimal(Decimal(GRIDPERC)/Decimal(100)*currentprice)
 step = 1
-
+isAPIAvailable = True
 stepprice = [currentprice-stepsize]
 orderidlist = []
 enoughBalance = False
@@ -72,87 +72,109 @@ def startup():
         else: print("Not sufficient funds to create buy orders") 
             
             
+def connectivityCheck():
+    print("Checking connectivity to binance API...")
+    try:
+        global isAPIAvailable
+        client.ping()
+        isAPIAvailable = True
+    except:
+        print("Binance API not available")
+        global isAPIAvailable
+        isAPIAvailable = False
+        
+
+
     
 startup()
 
 def job():
-    balanceChecker()
-    print("Checking if orders have been filled...")
-    if len(orderidlist) != 0:
-        order = client.get_order(
-    symbol=SYMBOL,
-    origClientOrderId =str(orderidlist[0]))
-        if(order['status'] == 'FILLED'):
-            print('Order filled, calculating sell price...')
-            try:
-                neworder = client.order_limit_sell(
-                symbol=SYMBOL,
-                quantity=QUANTITY,
-                price=round(round_to_tenths[0]*Decimal(1+(GRIDPERC/100)),2))
-                logger.info('Trying to remove id from list: ' + 'ORDERIDLIST: '+str(orderidlist)+' unique ID to remove: ' + str(Decimal(order['clientOrderId']) ))
-                round_to_tenths.remove(round(Decimal(order['price']),2))
-                orderidlist.remove(Decimal(order['clientOrderId'])) 
-            except: 
-                logging.exception('')
-                logger.info('Error, please investigate price: '+ str(round(round_to_tenths[0]*Decimal(1+(GRIDPERC/100)),2)))
-    
-    print("Checking if bot can create new buy orders...")
-    print(str(orderidlist))
-    if enoughBalance == True:
-        if len(orderidlist) < GRIDS:
-                r1 = random.randint(0, 100000)
-                price = round((min(round_to_tenths))-stepsize,2)
-                neworder = client.order_limit_buy(
-                symbol=SYMBOL,
-                quantity=QUANTITY,
-                price=price,
-                newClientOrderId = str(r1)
-                )
-                orderidlist.append(r1)
-                print("Appended it now, "+str(orderidlist))
-                round_to_tenths.append(price)
-    else: 
-        logger.info("Error with creating buy orders")
-        print("Not sufficient funds to create buy orders")
+    connectivityCheck()
+    if isAPIAvailable == True:
+        print("Binance API is available")
 
-    
-    print('Checking if orders are still inside range')
-    if 1 > 0:
+
+
+
+
+        balanceChecker()
+        print("Checking if orders have been filled...")
+
+
         try:
-            newprice = round(Decimal(client.get_symbol_ticker(symbol=SYMBOL)["price"])-stepsize,2)
-            logger.info("Bughunt, newprice is " + str(newprice) + "Round_to_tenths[0] is" + str(Decimal(round_to_tenths[0])) + "1+(Decimal(GRIDPERC)/100) is " + str(1+(Decimal(GRIDPERC)/100)))
-            if Decimal(newprice) > Decimal(round_to_tenths[0])*(1+(Decimal(GRIDPERC)/100)):
-                try:
-                    print('Cancelling lowest order and bringing it on top')
-                    client.cancel_order(
-                    symbol=SYMBOL,
-                    origClientOrderId=orderidlist[-1]
-                    )
-                    round_to_tenths.pop(-1)
-                    orderidlist.pop(-1)
+            if len(orderidlist) != 0:
+                order = client.get_order(
+            symbol=SYMBOL,
+            origClientOrderId =str(orderidlist[0]))
+                if(order['status'] == 'FILLED'):
+                    print('Order filled, calculating sell price...')
+                    try:
+                        neworder = client.order_limit_sell(
+                        symbol=SYMBOL,
+                        quantity=QUANTITY,
+                        price=round(round_to_tenths[0]*Decimal(1+(GRIDPERC/100)),2))
+                        logger.info('Trying to remove id from list: ' + 'ORDERIDLIST: '+str(orderidlist)+' unique ID to remove: ' + str(Decimal(order['clientOrderId']) ))
+                        round_to_tenths.pop(0)
+                        orderidlist.pop(0)
+                    except: 
+                        logging.exception('')
+                        logger.info('Error, please investigate price: '+ str(round(round_to_tenths[0]*Decimal(1+(GRIDPERC/100)),2)))
+        except: logger.exception('')
+
+        
+        print("Checking if bot can create new buy orders...")
+        print(str(orderidlist))
+        if enoughBalance == True:
+            if len(orderidlist) < GRIDS:
                     r1 = random.randint(0, 100000)
+                    price = round((min(round_to_tenths))-stepsize,2)
                     neworder = client.order_limit_buy(
                     symbol=SYMBOL,
                     quantity=QUANTITY,
-                    price=newprice,
-                    newClientOrderId = str(r1))
-                    orderidlist.insert(0,r1)
-                    round_to_tenths.insert(0,newprice)
-                except:
-                    print("Order doesnt exist")
-                    logging.exception('')
-        except:
-            logging.exception('')
+                    price=price,
+                    newClientOrderId = str(r1)
+                    )
+                    orderidlist.append(r1)
+                    print("Appended it now, "+str(orderidlist))
+                    round_to_tenths.append(price)
+        else: 
+            logger.info("Error with creating buy orders")
+            print("Not sufficient funds to create buy orders")
+
+        
+        print('Checking if orders are still inside range')
+        if 1 > 0:
+            try:
+                newprice = round(Decimal(client.get_symbol_ticker(symbol=SYMBOL)["price"])-stepsize,2)
+                logger.info("Bughunt, newprice is " + str(newprice) + "Round_to_tenths[0] is" + str(Decimal(round_to_tenths[0])) + "1+(Decimal(GRIDPERC)/100) is " + str(1+(Decimal(GRIDPERC)/100)))
+                if Decimal(newprice) > Decimal(round_to_tenths[0])*(1+(Decimal(GRIDPERC)/100)):
+                    try:
+                        print('Cancelling lowest order and bringing it on top')
+                        client.cancel_order(
+                        symbol=SYMBOL,
+                        origClientOrderId=orderidlist[-1]
+                        )
+                        round_to_tenths.pop(-1)
+                        orderidlist.pop(-1)
+                        r1 = random.randint(0, 1000000)
+                        neworder = client.order_limit_buy(
+                        symbol=SYMBOL,
+                        quantity=QUANTITY,
+                        price=newprice,
+                        newClientOrderId = str(r1))
+                        orderidlist.insert(0,r1)
+                        round_to_tenths.insert(0,newprice)
+                    except:
+                        print("Order doesnt exist")
+                        logging.exception('')
+            except:
+                logging.exception('')
 
 
 
 
 
 
-def getlatesttradetime():
-    try:
-        return str(client.get_historical_trades(symbol=SYMBOL,limit=1)[0]['time'])
-    except: pass
     
 
 
